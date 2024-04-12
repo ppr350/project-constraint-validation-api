1ff6b9425d106d8acbb4a1ab51201c1f136a3dac
Chee Hong "Kobe" Wong <kobito610@gmail.com>
2024-04-12 12:28:32 +0100

    Working project

--- src/index.js
+++ src/index.js
@@ -1,2 +1,21 @@
 import _ from 'lodash';
 import './style.css';
+import { validateForm } from "./validate"
+
+const form = document.querySelector('form')
+const email = document.getElementById('email')
+const emailError = document.querySelector('#email + span.error')
+const country = document.getElementById('country')
+const countryError = document.querySelector('#country + span.error')
+const zipCode = document.getElementById('zip-code')
+const zipCodeError = document.querySelector('#zip-code + span.error')
+const password = document.getElementById('password')
+const passwordError = document.querySelector('#password + span.error')
+const confirmPassword = document.getElementById('confirm-password')
+const confirmPasswordError = document.querySelector('#confirm-password + span.error')
+
+window.addEventListener('load', () => {
+    validateForm()
+})
+
+export { form, email, emailError, country, countryError, zipCode, zipCodeError, password, passwordError, confirmPassword, confirmPasswordError }
